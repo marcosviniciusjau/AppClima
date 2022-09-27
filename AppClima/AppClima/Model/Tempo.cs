a5d635871c9889d80a096ffcbd72e662edd3d973
Usuario <Usuario@CPD03-M08>
2022-09-27 11:37:33 -0300

    Adicionar arquivos de projeto.

---
+++ AppClima/AppClima/Model/Tempo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppClima.Model
+{
+     class Tempo
+    {
+        public string Title { get; set; }
+        public string Temperatura { get; set; }
+        public string Vento { get; set; }
+        public string Humidade { get; set; }
+        public string Visibilidade { get; set; }
+        public string Nascer_Sol { get; set; }
+        public string Por_Sol { get; set; }
+
+        public Tempo()
+        {
+            this.Title = "";
+            this.Temperatura = "";
+            this.Vento = "";
+            this.Humidade = "";
+            this.Visibilidade = "";
+            this.Nascer_Sol = "";
+            this.Por_Sol = "";
+        }
+    }
+}
