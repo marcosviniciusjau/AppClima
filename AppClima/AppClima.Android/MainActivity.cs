a7cf816372a669ca8ed0563ca80616c8997fb21b
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2023-02-25 16:41:36 -0300

    front-end

--- AppClima/AppClima.Android/MainActivity.cs
+++ AppClima/AppClima.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace AppClima.Droid
 {
-    [Activity(Label = "AppClima", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Clima", Icon = "@mipmap/clima_icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
